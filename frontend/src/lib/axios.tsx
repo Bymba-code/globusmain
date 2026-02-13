7e4578a862fdc4bf2f078d76bbcbb4965e811414
Bymba-code <bymba6607@gmail.com>
2026-02-13 08:23:42 +0800

    updated

--- frontend/src/lib/axios.tsx
+++ frontend/src/lib/axios.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_REACT_APP_API_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
